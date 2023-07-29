183031912fa04921cca51064ac4cfa69ae0643d9
Sergiusz Gołacki <47930579+xMann999@users.noreply.github.com>
2023-07-29 20:01:43 +0200

    Delete medicalClinicDetailsApp directory

--- medicalClinicDetailsApp/src/main/java/com/sergiuszg/medicalClinicDetailsApp/model/Specialisation.java
+++
@@ -1,6 +0,0 @@
-package com.sergiuszg.medicalClinicDetailsApp.model;
-
-public enum Specialisation {
-
-    ANESTHESIOLOGY, DERMATOLOGY, RADIOLOGY, FAMILY_MEDICINE, NEUROLOGY, PEDIATRICS, ONCOLOGY, UROLOGY, PATHOLOGY, SURGERY
-}
